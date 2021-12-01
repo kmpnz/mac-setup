3f6459f77bc3ee437e1d8eb77c029668c3c21000
Andreas Kompanez <kmpnz@Ambrosia.local>
2021-12-01 15:31:01 +0100

    Adds desktop icons hidding

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.dock autohide -boolean true 
+defaults write com.apple.finder CreateDesktop -bool false
 killall Dock
+killall Finder
